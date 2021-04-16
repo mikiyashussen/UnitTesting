f1e504704ecc575f4654c34b18169f4d9b186214
mikiyashussen <mikiyashussen19@gmail.com>
2021-04-16 21:01:39 +0200

    first commit

---
+++ QuadraticEqnRootsCal.UnitTest/QuaraticEqnTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEqnRootsCal.UnitTest
+{
+    [TestFixture]
+    class QuaraticEqnTests
+    {
+
+        [Test]
+        public void RootsCalculator_HasTwoRoots_ReturnsTwoRoots()
+        {
+            // Assign
+            var quadraticEqn = new QuadraticEqn(1, 3, 2);
+
+
+            // Act
+            List<double> result;
+            result = quadraticEqn.RootsCalculator();
+
+            // Assert
+            Assert.That(result[0] == -1);
+            Assert.That(result[1] == -2);
+        }
+
+        [Test]
+        public void RootsCalculator_HasOneRoot_ReturnsOneRoot()
+        {
+            // Assign
+            var quadraticEqn = new QuadraticEqn(1, 4, 4);
+
+            // Act
+            List<double> result;
+            result = quadraticEqn.RootsCalculator();
+
+            // Assert
+            Assert.That(result[0] == -2);
+        }
+
+        [Test]
+        public void RootsCalculator_HasNoRoots_ReturnsNoRoot()
+        {
+            // Assign
+            var quadraticEqn = new QuadraticEqn(1, 2, 4);
+
+            // Act
+            List<double> result;
+            result = quadraticEqn.RootsCalculator();
+
+            // Assert
+            Assert.That(result[0] == 0);
+        }
+    }
+}
---
+++ QuadraticEqnRootsCal/QuadraticEqn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEqnRootsCal
+{
+    public class QuadraticEqn
+    {
+        public static int a;
+        public static int b;
+        public static int c;
+        public static double delta;
+        public QuadraticEqn(int x, int y, int z)
+        {
+            a = x;
+            b = y;
+            c = z;
+            delta = CalDelta(); 
+        }
+
+        public double CalDelta()
+        {
+            return Math.Sqrt((Math.Pow(b, 2)) - (4 * a * c));
+        }
+        public List<double> RootsCalculator()
+        {   
+            if (delta > 0)
+            {              
+                return CalTwoRoots();
+            }
+            else if (delta == 0)
+            {
+                return CalOneRoot();
+            }
+            else
+            {
+                return new List<double> { 0 };
+            }
+
+        }
+
+        public List<double> CalTwoRoots()
+        {
+            
+            double root1 = (-b + delta) / (2 * a);
+            double root2 = (-b - delta) / (2 * a);
+
+            return new List<double> { root1, root2};
+        }
+
+        public List<double> CalOneRoot()
+        {
+            double root = (-b / (2 * a));
+            return new List<double> { root };
+        }
+    }
+}
